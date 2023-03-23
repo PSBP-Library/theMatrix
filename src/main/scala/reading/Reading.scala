26993afdce36e22b4fd305f5e8a76f47765e9e17
PSBP-Library <luc.duponcheel@gmail.com>
2023-03-23 11:54:48 +0100

    reading and writing uniform now

--- src/main/scala/reading/Reading.scala
+++ src/main/scala/reading/Reading.scala
@@ -7,4 +7,7 @@ import CSVReader.open
 
 import types.{Matrix}
 
-val reading: File => Unit => Matrix[String] = file => _ => open(file).all()
+val reading: File => Unit => Matrix[String] = file =>
+  _ =>
+    val matrix = open(file).all()
+    matrix
